b9f51cf738f55805bb3e55a025057d099f234c5b
Hien205 <phuonghien29032005@gmail.com>
2026-09-21 14:18:59 +0700

    add capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+﻿import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hien205.vkujobsurvey',
+  appName: 'VKU Student Job Survey',
+  webDir: 'dist',
+};
+
+export default config;
